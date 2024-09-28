8d79013278b36f08c56cf3f89349df4a1b8ebf81
pointThink <69512713+pointThink@users.noreply.github.com>
2024-09-28 18:10:46 +0200

    Text formatting

--- src/internal/post.php
+++ src/internal/post.php
@@ -22,6 +22,46 @@ class Post
 
 	public $replies = array();
 
+	public function format_and_show_text($text)
+	{
+		$text = htmlspecialchars($text);
+
+		$text = preg_replace('#(script|about|applet|activex|chrome):#is', "\\1:", $text);
+
+		$ret = ' ' . $text;
+	
+		// Replace Links with http://
+		$ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\" rel=\"nofollow\">\\2</a>", $ret);
+	
+		// Replace Links without http://
+		$ret = preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\" rel=\"nofollow\">\\2</a>", $ret);
+
+		// Replace Email Addresses
+		$ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
+		$ret = substr($ret, 1);
+
+		// bold
+    	$ret = preg_replace('/\*\*(.+)\*\*/sU', '<b>$1</b>', $ret);
+    	// italic
+	    $ret = preg_replace('/\*(.+)\*/sU', '<i>$1</i>', $ret);
+
+		$textParts = explode("\n", $ret);
+
+		echo "<div class=post_text>\n";
+    	foreach ($textParts as $part)
+    	{
+        	if (str_starts_with($part, "&gt"))
+            	echo "<pre class='greentext'>$part</pre>";
+        	else if (str_starts_with($part, "&lt"))
+            	echo "<pre class='orangetext'>$part</pre>";
+        	else if (str_starts_with($part, "^"))
+            	echo "<pre class='bluetext'>$part</pre>";
+        	else
+            	echo "<pre>$part</pre>";
+    	}
+    	echo "</div>\n";
+	}
+
 	public function display()
 	{
 		echo "<div class=post>";
@@ -31,7 +71,7 @@ class Post
 		
 		echo "<a href=/$this->board/post.php?id=$this->id><p class=post_id>>$this->id | $this->creation_time</p></a>";
 		echo "<h4>$this->title</h4>";
-		echo "<pre>$this->body</pre>";
+		$this->format_and_show_text($this->body);
 
 		foreach ($this->replies as $reply)
 			$reply->display_reply();
