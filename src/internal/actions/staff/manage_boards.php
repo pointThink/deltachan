59c99b9b1cc7af45e749df99ec33dc6fe6ab4481
pointThink <69512713+pointThink@users.noreply.github.com>
2024-09-29 16:47:28 +0200

    Manage boards layout

---
+++ src/internal/actions/staff/manage_boards.php
@@ -0,0 +1,51 @@
+<?php
+include_once "../../database.php";
+include_once "../../staff_session.php";
+
+if (!staff_session_is_valid())
+	die("You are not allowed here");
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Manage boards</title>
+		<?php include "../../link_css.php" ?>
+	</head>
+
+	<body>
+		<?php include "../../../topbar.php" ?>
+		
+		<h1 class=title>Manage boards</h1>
+
+		<table class=manage_table>
+			<tr>
+				<th>ID</th>
+				<th>Title</th>
+				<th>Subtitle</th>
+				<th>Edit</th>
+			</tr>
+
+			<?php
+				$database = new Database();
+				$boards = $database->get_boards();
+
+				foreach ($boards as $board)
+				{
+					echo "
+					<tr>
+						<td>$board->id</td>
+						<td>$board->title</td>
+						<td>$board->subtitle</td>
+						<td><a href=#>Edit (TODO)</a></td>
+					</tr>
+					";
+				}
+			?>
+		</table>
+
+		<a class=manage_link href=#>(TODO) Add board</a>
+
+		<?php include "../../../footer.php" ?>
+	</body>
+</html>
