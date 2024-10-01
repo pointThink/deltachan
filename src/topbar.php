063a6f75c43a7ad1ab1fec6d79f60a48a02148b0
pointThink <69512713+pointThink@users.noreply.github.com>
2024-10-01 21:40:16 +0200

    Topbar style changes

--- src/topbar.php
+++ src/topbar.php
@@ -1,9 +1,10 @@
 <div id=topbar>
+	<p>[</p>
 	<a href=/>home</a>
 	<a href=/rules.php>rules</a>
+	<p>]</p>
 
-	<p>|</p>
-
+	<p>[</p>
 	<?php
 		include_once "internal/database.php";
 	
@@ -11,6 +12,9 @@
 		$boards = $db->get_boards();
 
 		foreach ($boards as $b)
+		{
 			echo "<a href=/$b->id/>$b->id</a>";
+		}
 	?>
+	<p>]</p>
 </div>
